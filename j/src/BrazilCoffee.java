14a6227c630176e2520aa94789aad9c67a09e634
junghoyeon <yeon428@naver.com>
2016-09-21 12:26:30 +0900

    2-3

---
+++ j/src/BrazilCoffee.java
@@ -0,0 +1,17 @@
+﻿class BrazilCoffee{
+String origin;
+int degreeOfRoast;
+int grade;
+public BrazilCoffee(String o){
+this.origin=o;
+}
+public void roast(){
+System.out.println("Roasting!"+origin);
+}
+public void grind(){
+System.out.println("Grinding!"+origin);
+}
+public void brew(){
+System.out.println("Brewing!"+origin);
+}
+}
